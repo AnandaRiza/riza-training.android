f1a75039a91735e284152b1b75ee09250283e7d8
Ananda Riza <87215017+AnandaRiza@users.noreply.github.com>
2024-03-22 14:16:15 +0700

    progress 22/03/2024

---
+++ app/src/main/java/com/example/bcasyariah/data/Service.kt
@@ -0,0 +1,11 @@
+package com.example.bcasyariah.data
+
+import com.example.bcasyariah.model.MenuDashboardModel
+import com.example.bcasyariah.model.MenuDashboardResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface Service {
+    @GET("menu-dashboard")
+    suspend fun getHomeMenu(): Response<MenuDashboardResponse>
+}
